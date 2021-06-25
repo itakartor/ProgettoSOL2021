cd2ceae0e29f0de8246665d58a5f7ec55204f8af
itakartor <carmine1403@gmail.com>
2021-06-25 08:53:12 +0200

    modifiche per il nuovo file util

--- Parser.c
+++ Parser.c
@@ -5,23 +5,12 @@
 #include <getopt.h>
 
 #include "queue.h" //ho incluso la definizione di coda
+#include "util.h"
 
 // limite sulla lunghezza della stringa passata come argomento
 #define MY_ARGV_MAX  512
 
-int isNumber(const char* s, int* n) {
-  if (s==NULL) return 1;
-  if (strlen(s)==0) return 1;
-  char* e = NULL;
-  errno=0;
-  long val = strtol(s, &e, 10);
-  if (errno == ERANGE) return 2;    // overflow
-  if (e != NULL && *e == (char)0) {
-    *n = val;
-    return 0;   // successo 
-  }
-  return 1;   // non e' un numero
-}
+
 
 int arg_h()
 {
