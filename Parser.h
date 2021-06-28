35ed00d4c1efa17b78a1d358f872149aa71674aa
itakartor <carmine1403@gmail.com>
2021-06-28 15:09:34 +0200

    aggiunta del file parser.h e parser nel client

---
+++ Parser.h
@@ -0,0 +1,1 @@
+Queue* parser(char* argv[],int argc);
---
+++ client.c
@@ -0,0 +1,164 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <dirent.h>
+#include <getopt.h>
+#include <time.h>
+#include <errno.h>
+#include <limits.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/socket.h>
+#include <sys/uio.h>
+
+#include <sys/un.h>
+#include <ctype.h>
+#include <pthread.h>
+
+#include "queue.h"
+#include "util.h"
+#include "Parser.h"
+
+#define SOCKNAME "mysock"
+long sockfd;
+
+static int add_to_current_time(long sec, long nsec, struct timespec* res)
+{
+    // TODO: maybe check its result
+    clock_gettime(CLOCK_REALTIME, res);
+    res->tv_sec += sec;
+    res->tv_nsec += nsec;
+
+    return 0;
+}
+
+int set_timespec_from_msec(long msec, struct timespec* req) 
+{
+  if(msec < 0 || req == NULL)
+  {
+    errno = EINVAL;
+    return -1;
+  }
+
+  req->tv_sec = msec / 1000;
+  msec = msec % 1000;
+  req->tv_nsec = msec * 1000;
+
+  return 0;
+}
+
+int openConnection(const char* sockname, int msec, const struct timespec abstime) 
+{
+    if(sockname == NULL || msec < 0) 
+    { //argomenti non validi
+        errno = EINVAL;
+        return -1;
+    }
+
+    struct sockaddr_un serv_addr;
+    SYSCALL_EXIT("socket", sockfd, socket(AF_UNIX, SOCK_STREAM, 0), "socket", "");
+    memset(&serv_addr, '0', sizeof(serv_addr));
+
+    serv_addr.sun_family = AF_UNIX;
+    strncpy(serv_addr.sun_path,SOCKNAME, strlen(SOCKNAME)+1);
+
+    // setting waiting time
+    struct timespec wait_time;
+    // no need to check because msec > 0 and &wait_time != NULL
+    set_timespec_from_msec(msec, &wait_time);
+
+    // setting current time
+    struct timespec curr_time;
+    clock_gettime(CLOCK_REALTIME, &curr_time);
+
+    // trying to connect
+    int err = -1;
+    fprintf(stderr, "currtime %ld abstime %ld\n", curr_time.tv_sec, abstime.tv_sec);
+    while( (err = connect(sockfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr))) == -1
+            && curr_time.tv_sec < abstime.tv_sec ){
+        //debug("connect didn't succeed, trying again...\n");
+        fprintf(stderr, "err4 %d\n", errno);
+
+        if( nanosleep(&wait_time, NULL) == -1){
+            sockfd = -1;
+            return -1;
+        }
+        if( clock_gettime(CLOCK_REALTIME, &curr_time) == -1){
+            sockfd = -1;
+            return -1;
+        }
+    }
+
+    if(err == -1) {
+        //debug("Could not connect to server. :(\n");
+
+        sockfd = -1;
+        errno = ETIMEDOUT;
+        return -1;
+    }
+
+    //debug("Connected! :D\n");
+
+    //socket_path = sockname;
+    fprintf(stderr, "mi sono connesso al server!\n");
+    return 0;
+}
+
+int EseguiComandoClient(NodoComando *tmp) {
+  int notused;
+  char *buffer = NULL;
+  if(tmp == NULL) return -1; //errore: tmp non può e non deve essere NULL. Abbiamo già controllato che q->len > 0
+  char* towrite = malloc(sizeof(char) * (strlen(tmp->name) + 1)); //alloco la stringa da scrivere, che sarà del tipo "rfile"
+  towrite[0] = tmp->cmd;
+  
+  for(int i = 1; i <= strlen(tmp->name); i++) //vado a prendere la stringa della richiesta che è stata suddivisa dal parser
+    towrite[i] = tmp->name[i - 1];
+
+  //fprintf(stderr, "sto scrivendo nel socket %s\n", towrite);
+  int n = strlen(towrite) + 1; //+1 per il terminatore
+
+  SYSCALL_EXIT("writen", notused, writen(sockfd, &n, sizeof(int)), "write", "");          //scrivo nel socket cosa ho estratto dalla coda
+  SYSCALL_EXIT("writen", notused, writen(sockfd, towrite, n * sizeof(char)), "write", "");
+
+  buffer = realloc(buffer, n*sizeof(char));
+  if (!buffer) { perror("realloc"); fprintf(stderr, "Memoria esaurita....\n"); }
+
+
+  SYSCALL_EXIT("readn", notused, readn(sockfd, &n, sizeof(int)), "read", "");             //leggo una possibile risposta del server 
+  //fprintf(stderr, "e fin qui\n");
+  SYSCALL_EXIT("readn", notused, readn(sockfd, buffer, n * sizeof(char)), "read", "");
+  buffer[n] = '\0';
+  fprintf(stderr,"result: %s\n", buffer);                                                         //stampo a video cosa ricevo dal server
+}
+
+
+int main(int argc, char *argv[]) 
+{
+  Queue *q = parser(argv,argc); //coda delle operazioni
+  struct timespec abstime;
+
+  add_to_current_time(2, 0, &abstime);
+  //primo parametro: tempo limite (in secondi)
+  //secondo parametro: intervallo di tempo tra due connessioni (in millisecondi)
+
+  /*struct sockaddr_un serv_addr;
+  int sockfd;
+  SYSCALL_EXIT("socket", sockfd, socket(AF_UNIX, SOCK_STREAM, 0), "socket", "");
+  memset(&serv_addr, '0', sizeof(serv_addr));
+  serv_addr.sun_family = AF_UNIX;
+  strncpy(serv_addr.sun_path,SOCKNAME, strlen(SOCKNAME)+1);
+  int notused;
+  SYSCALL_EXIT("connect", notused, connect(sockfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)), "connect", "");*/
+  int x = openConnection(SOCKNAME, 0, abstime); //da vedere se da errore
+  fprintf(stderr, "sockfd: %ld, risultato openconnection %d\n", sockfd, x);
+
+
+  while(q->len > 0) { //finchè ci sono richieste che il parser ha visto
+    NodoComando *tmp = pop(&q);
+    EseguiComandoClient(tmp);
+
+  }
+  
+  return 0;
+}
