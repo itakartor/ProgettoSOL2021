76140213c9ed7096191672b6e261f5608b4e81ee
itakartor <58552414+itakartor@users.noreply.github.com>
2021-07-01 17:17:06 +0200

    commenti utili e W riscritto meglio

--- client.c
+++ client.c
@@ -102,8 +102,6 @@ int openConnection(const char* sockname, int msec, const struct timespec abstime
     }
 
     //debug("Connected! :D\n");
-
-    //socket_path = sockname;
     fprintf(stderr, "mi sono connesso al server!\n");
     return 0;
 }
@@ -136,24 +134,22 @@ int writeCMD(const char* pathname, char cmd)
   //manca gestione errore
   int notused;
   char *buffer = NULL;
-  char* towrite = malloc(sizeof(char) * (strlen(pathname) + 2)); //alloco la stringa da scrivere, che sarà del tipo "rfile"
+  char* towrite = malloc(sizeof(char) * (strlen(pathname) + 2)); //alloco la stringa per la stringa comando da mandare al server con terminatore
   towrite[0] = cmd;
   for(int i = 1; i <= strlen(pathname); i++)
     towrite[i] = pathname[i - 1];
-  towrite[strlen(pathname) + 1] = '\0';
-  fprintf(stderr, "sto scrivendo nel socket %s, nome file originale %s\n", towrite, pathname);
+  towrite[strlen(pathname) + 1] = '\0';//metto il terminatore
+  fprintf(stderr, "sto scrivendo nel socket %s, nome file originale %s\n", towrite, pathname); //debug
   int n = strlen(towrite) + 1; //terminatore
 
-  SYSCALL_EXIT("writen", notused, writen(sockfd, &n, sizeof(int)), "write", "");
+  SYSCALL_EXIT("writen", notused, writen(sockfd, &n, sizeof(int)), "write", ""); //scrivo il comando "semplice" al server
   SYSCALL_EXIT("writen", notused, writen(sockfd, towrite, n * sizeof(char)), "write", "");
-
 }
 
 int removeFile(const char* pathname) 
 {
-  writeCMD(pathname,'c');//utilizzo il comando c 
-
-  int res, notused;
+  writeCMD(pathname,'c');//utilizzo il comando 'c' 
+  int res, notused;//res è il risultato del server per controllare le corrette condizioni di esecuzione
   SYSCALL_EXIT("readn", notused, readn(sockfd, &res, sizeof(int)), "read", "");
   if(res != 1) { perror("ERRORI RIMOZIONE"); return -1; }//controllo se ho rimosso in modo corretto il file
   fprintf(stderr, "File %s cancellato con successo dal server\n", pathname);//debug 
@@ -165,22 +161,22 @@ int removeFile(const char* pathname)
 int readFile(const char* pathname, void** buf, int* size)//leggo il file al pathname e poi metto il contenuto nel buf
 {
   fprintf(stderr, "pathname a questo punto %s\n", pathname);
-  writeCMD(pathname, 'r');//scrivo il comando al server
+  writeCMD(pathname, 'r');//utilizzo il comando 'r'
   int notused;
-  int n;
+  int n;//intermediario pe il size
   SYSCALL_EXIT("readn", notused, readn(sockfd, &n, sizeof(int)), "read", "");
   *size = n; //ho letto la size del file
   //fprintf(stderr, "la size del file che sto provando ad allocare è %d\n", n);
   if(*size == -1) //se non riesco a trovare il file il server scriverà al client -1 per indicare l'errore
   {
-    fprintf(stderr, "file %s non esiste\n", pathname);
+    fprintf(stderr, "file %s non esiste\n", pathname);//debug
     *buf = NULL;//metto il buff a NULL per evitare che provi a scrivere qualcosa nel buf
     *size = 0;
-    return -1; //errore
+    return -1; //errore o file inesistente
   } 
   else 
   {
-    *buf = malloc(sizeof(char) * n);
+    *buf = malloc(sizeof(char) * n);//alloco lo spazio del buffer per il contenuto del file
     SYSCALL_EXIT("readn", notused, readn(sockfd, *buf, n * sizeof(char)), "read", "");//leggo il contenuto del file
     fprintf(stderr, "ho letto il file %s dal server\n", pathname);
     //fprintf(stderr, "ho letto il file %s con contenuto\n %s\n", pathname, (char*)(*buf));
@@ -190,52 +186,53 @@ int readFile(const char* pathname, void** buf, int* size)//leggo il file al path
 
 int appendToFile(const char* pathname, void* buf, int size) 
 {
-  FILE *f = fopen (pathname, "a"); //gestire errori
+  FILE *f = fopen (pathname, "a"); //bisogna gestire gli errori 
   fwrite(buf, 1, size, f);
   fclose(f);
   return 0;//successo
 }
 
-int readNFiles(int n, const char* dirname) {
+int readNFiles(int n, const char* dirname) //int n è il numero dei file da leggere
+{
   //DA FARE: CONTROLLARE SE DIRNAME = NULL, NEL CASO DARE ERRORE
-  char* ntmp = malloc(sizeof(char) * 10);
-  sprintf(ntmp, "%d", n);
+  char* ntmp = malloc(sizeof(char) * 10); //passo il numero come un carattere per riutilizzare il codice
+  sprintf(ntmp, "%d", n);//memorizzo il numero in ntmp
   writeCMD(ntmp, 'R');
 
   int notused;
-  SYSCALL_EXIT("readn", notused, readn(sockfd, &n, sizeof(int)), "read", "");
-  int lenpathtmp;
+  SYSCALL_EXIT("readn", notused, readn(sockfd, &n, sizeof(int)), "read", "");//leggo dal socket del server il numero di file da leggere
+  int lenpathtmp;                                                            //visto che potrebbero essere meno di quelli dichiarati dal parametro n
   char* buftmp;
   char** arr_buf = malloc(sizeof(char*) * n); //abbiamo messo l'array perchè sennò fa casino con le read della readFile
-  for(int i = 0; i < n; i++) { //per ogni file da leggere dal server
+  for(int i = 0; i < n; i++) 
+  { //per ogni file da leggere dal server
     SYSCALL_EXIT("readn", notused, readn(sockfd, &lenpathtmp, sizeof(int)), "read", "");
-    buftmp = malloc(sizeof(char) * lenpathtmp);
+    buftmp = malloc(sizeof(char) * lenpathtmp);//passaggio in più inutile
     SYSCALL_EXIT("readn", notused, readn(sockfd, buftmp, lenpathtmp * sizeof(char)), "read", "");
     //fprintf(stderr, "leggo il file %s dal server\n", buftmp);
-    arr_buf[i] = buftmp;
-    //void* buffile;
-    //int sizebufffile;
-    //readFile(buftmp, &buffile, &sizebufffile);
-  }
-  fprintf(stderr, "\n\n\n STO STAMPANDO L'ARRAY \n\n\n");
-  for(int i = 0; i < n; i++) {
-    fprintf(stderr, "elemento %d: %s\n", i, arr_buf[i]);
+    arr_buf[i] = buftmp;//passaggio in più inutile posso direttamente allocare l'array e fargli salvare il contenuto in esso
+  } 
+  fprintf(stderr, "\n\n\n STO STAMPANDO L'ARRAY \n\n\n");//debug
+  //prima mi ricavo tutti i nomi dei file da leggere e poi li leggo per evitare conflitti
+  //nelle scritture/letture dei socket
+  for(int i = 0; i < n; i++) 
+  {
+    fprintf(stderr, "elemento %d: %s\n", i, arr_buf[i]);//debug
     void* buffile;
     int sizebufffile;
-    readFile(arr_buf[i], &buffile, &sizebufffile);
-    char path[1024];
-    fprintf(stderr, "fin qui ci siamo\n");
+    readFile(arr_buf[i], &buffile, &sizebufffile);//leggo un singolo file preso dall'array dei file
+    char path[1024];//dichiaro un path 
+    //fprintf(stderr, "fin qui ci siamo\n");
     snprintf(path, sizeof(path), "%s/%s", dirname, arr_buf[i]);
-    fprintf(stderr, "fin qui ci siamo, dirname %s, path %s\n", dirname, path);
+    fprintf(stderr, "fin qui ci siamo, dirname %s, path %s\n", dirname, path);//debug
 
-    appendToFile(path, buffile, sizebufffile);
+    appendToFile(path, buffile, sizebufffile);//infine scrivo in append il contenuto del file
   }
-  //SYSCALL_EXIT("writen", notused, writen(sockfd, &n, sizeof(int)), "write", "");
 }
 
 int writeFile(const char* pathname) //scrivo un file nel server 
 {
-  int notused, n;
+  int notused, n;//n è la lunghezza del buffer nel quale salvo le risposte del server 
   writeCMD(pathname, 'W');
   char *buffer = NULL;
   n = strlen(pathname) + 2; //+2 per il comando e il terminatore
@@ -252,7 +249,7 @@ int writeFile(const char* pathname) //scrivo un file nel server
   {
     //fprintf(stderr, "file ok\n");
     FILE * f = fopen (pathname, "rb");
-
+    //questa parte recupera la lunghezza e il contenuto di un file potrei pure metterlo in una funzione
     long length;
     char* bufferFile;
     if (f) //copia il file in un buffer
@@ -268,7 +265,7 @@ int writeFile(const char* pathname) //scrivo un file nel server
       fclose (f);
     }
 
-    fprintf(stderr, "length file %s: %ld\n", pathname, length);
+    fprintf(stderr, "length file %s: %ld\n", pathname, length);//debug
     SYSCALL_EXIT("writen", notused, writen(sockfd, &length, sizeof(int)), "write", "");
     int cista;//se il file entra nel server per la capienza o il numero dei file
     SYSCALL_EXIT("readn", notused, readn(sockfd, &cista, sizeof(int)), "read", "");//leggo se il file può essere caricato nel server per capienza o nFile
@@ -285,7 +282,7 @@ int writeFile(const char* pathname) //scrivo un file nel server
 
 
     SYSCALL_EXIT("readn", notused, readn(sockfd, &n, sizeof(int)), "read", "");
-    SYSCALL_EXIT("readn", notused, readn(sockfd, buffer, n * sizeof(char)), "read", "");
+    SYSCALL_EXIT("readn", notused, readn(sockfd, buffer, n * sizeof(char)), "read", "");//leggo la risposta dal server
     buffer[n] = '\0';
     printf("result: %s\n", buffer);
   } 
@@ -297,6 +294,11 @@ int writeFile(const char* pathname) //scrivo un file nel server
 
 int EseguiComandoClient(NodoComando *tmp) 
 {
+  if(tmp == NULL)
+  {
+      perror("tmp NULL");
+      return -1; //errore: tmp non può e non deve essere NULL. 
+  }
   if(tmp->cmd == 'c')//gestisco il comando con c per eliminare il file dal server
   {
     fprintf(stderr, "file da rimuovere %s\n", tmp->name);
@@ -305,26 +307,31 @@ int EseguiComandoClient(NodoComando *tmp)
   }
   else
   { 
-    if(tmp->cmd == 'r') 
+    if(tmp->cmd == 'r')
     {
-      fprintf(stderr, "sto eseguendo sta parte\n");
+      fprintf(stderr, "sto eseguendo r\n");//debug
       void* buf;
       int sizebuff; //col size_t non va
       readFile(tmp->name, &buf, &sizebuff); //manca gestione errore
-      if(savefiledir != NULL && buf != NULL) 
+      if(savefiledir != NULL && buf != NULL) //se le condizioni sono corrette leggo il file
       {
         char path[1024];
         snprintf(path, sizeof(path), "%s/%s", savefiledir, tmp->name);
         //fprintf(stderr, "sto andando a scrivere il file %s in %s\n", tmp->name, path);
         appendToFile(path, buf, sizebuff);
+        return 0;
       }
-      return 0;
+      else // non ho una cartella per salvare il file oppure il buffer è vuoto(però se è vuoto dovrebbe ugualmente fare qualcosa)
+      {
+          perror("read");
+          return -1; //errore
+      }
     } 
     else
     {
-      if(tmp->cmd == 'R') 
+      if(tmp->cmd == 'R')//lettura di n file
       {
-        fprintf(stderr, "comando readNFiles con n = %d\n", tmp->n);
+        fprintf(stderr, "comando readNFiles con n = %d\n", tmp->n);//debug
         readNFiles(tmp->n, savefiledir);
         return 0;
       } 
@@ -332,79 +339,14 @@ int EseguiComandoClient(NodoComando *tmp)
       { 
         if(tmp->cmd == 'W') 
         {
-          fprintf(stderr, "comando W con parametro %s\n", tmp->name);
+          fprintf(stderr, "comando W con parametro %s\n", tmp->name);//debug
           writeFile(tmp->name);
         }
       }
     }
   }
-  int notused;
-  char *buffer = NULL;
-  if(tmp == NULL) return -1; //errore: tmp non può e non deve essere NULL. Abbiamo già controllato che q->len > 0
-  char* towrite = malloc(sizeof(char) * (strlen(tmp->name) + 1)); //alloco la stringa da scrivere, che sarà del tipo "rfile"
-  //parser semplice da passare alla coda delle richieste
-  towrite[0] = tmp->cmd;
-  
-  for(int i = 1; i <= strlen(tmp->name); i++) //vado a prendere la stringa della richiesta che è stata suddivisa dal parser
-    towrite[i] = tmp->name[i - 1];
-
-  //fprintf(stderr, "sto scrivendo nel socket %s\n", towrite);
-  int n = strlen(towrite) + 1; //+1 per il terminatore
-
-  SYSCALL_EXIT("writen", notused, writen(sockfd, &n, sizeof(int)), "write", "");          //scrivo nel socket cosa ho estratto dalla coda
-  SYSCALL_EXIT("writen", notused, writen(sockfd, towrite, n * sizeof(char)), "write", "");
-
-  if(tmp->cmd == 'W')//gestione del comando W
-  {
-    buffer = realloc(buffer, n*sizeof(char));
-    if (!buffer) { perror("realloc"); fprintf(stderr, "Memoria esaurita....\n"); }
-    
-    SYSCALL_EXIT("readn", notused, readn(sockfd, &n, sizeof(int)), "read", "");             //leggo una possibile risposta del server se il file che sto per inserire
-    SYSCALL_EXIT("readn", notused, readn(sockfd, buffer, n * sizeof(char)), "read", "");    //è gia stato inserito "file ok"-> file da inserire
-    buffer[n] = '\0';
-    fprintf(stderr, "buffer %s \n", buffer);                                                                       
-    if(strcmp(buffer, "file ok") == 0) //il file non esiste nel server
-    {
-      
-      FILE * f = fopen (tmp->name, "rb"); //apro il file in lettura e scrittura    
-
-      long length;
-      char* bufferFile;
-      
-      if (f) 
-      {//leggo la lunghezza/size del file e il suo contenuto
-        fseek (f, 0, SEEK_END);
-        length = ftell (f);
-        fseek (f, 0, SEEK_SET);
-        bufferFile = malloc(length); 
-        if(bufferFile)
-        {
-          fread(bufferFile, 1, length, f); //lettura del file e compiatura nel buffer
-        }
-        fclose (f);
-      }
-
-      SYSCALL_EXIT("writen", notused, writen(sockfd, &length, sizeof(int)), "write", "");             //scrivo nel socket del server il contenuto del file
-      SYSCALL_EXIT("writen", notused, writen(sockfd, bufferFile, length * sizeof(char)), "write", "");
-
-      buffer = realloc(buffer, n*sizeof(char));
-      if (!buffer) { perror("realloc"); fprintf(stderr, "Memoria esaurita....\n"); }
-
-
-      SYSCALL_EXIT("readn", notused, readn(sockfd, &n, sizeof(int)), "read", "");                     //leggo la risposta del server dal socket 
-      //fprintf(stderr, "e fin qui\n");
-      SYSCALL_EXIT("readn", notused, readn(sockfd, buffer, n * sizeof(char)), "read", "");
-      buffer[n] = '\0';
-      printf("result: %s\n", buffer); //stampo a video cosa ricevo dal server
-    }
-    else 
-    {
-      fprintf(stderr, "file già esistente nel server\n");                                              //se il file esiste gia nel server
-    }  
-  }                                                      
 }
-
-void visitaRicorsiva(char* name, int *n, Queue **q) 
+void visitaRicorsiva(char* name, int *n, Queue **q)//name è il nome del path e n è il numero dei file 
 { //alcuni file hanno problemi 
   //fprintf(stderr, "sto visitando %s\n", name);
   if(name == NULL)//se il path name non è corretto
