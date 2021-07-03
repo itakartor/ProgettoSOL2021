46aa146e50d250808811d0c6f742c7da3d4f4e8a
itakartor <carmine1403@gmail.com>
2021-07-03 09:49:31 +0200

    gestione server comandi

--- server.c
+++ server.c
@@ -38,6 +38,8 @@ fd_set set;         //maschera dei bit
 Queue *queueClient; //coda dei client che fanno richieste
 Queue *queueFiles; //coda dei file memorizzati
 
+int spazioOccupato = 0;
+
 int **p;            //array di pipe
 
 static pthread_mutex_t mutexQueueClient = PTHREAD_MUTEX_INITIALIZER;
@@ -121,6 +123,10 @@ void parserFile(void) {   //parser del file
   free(buffer);// libero l'array tmp, il buffer e chiudo il file
   //fprintf(stderr,"abbiamo chiuso il file\n");
   
+  if(spazio <= 0 || numeroFile <= 0 || SockName == NULL || numWorkers <= 0) {
+    fprintf(stderr, "config.txt errato\n");
+    exit(EXIT_FAILURE);
+  }
   //stampa di debug
   fprintf(stderr,"spazio: %d\n", spazio);
   fprintf(stderr,"numeroFile: %d\n", numeroFile);
@@ -167,70 +173,176 @@ static void* threadF(void* arg) //funzione dei thread worker
     {
       case 'W': 
       { //richiesta di scrittura
+        pthread_mutex_lock(&mutexQueueFiles);
         Node* esiste = fileExistsServer(queueFiles, parametro);
+        pthread_mutex_unlock(&mutexQueueFiles);
+
+        fileRam* newFile = esiste->data;
+
+          
+        //fprintf(stderr, "sto scrivendo\n");
+        SYSCALL_EXIT("readn", notused, readn(connfd, &(newFile->length), sizeof(int)), "read", "");//leggo la lunghezza del file 
+          
+        int cista = 1;
+        if(newFile->length > spazio) 
+        { //non lo memorizza proprio
+          fprintf(stderr, "Il file %s è troppo grande (%ld) e non sta materialmente nel server (capienza massima %d)\n", newFile->nome, newFile->length, spazio);
+            //vanno fatte delle FREE
+            //vado a scrivere nel socket che il file non ci sta
+          cista = 0;
+        }
+
+        if (writen(connfd, &cista, sizeof(int))<=0) { perror("c"); }
+        if(!cista)
+          continue;
         
-        if(esiste == NULL)//se fileExists ritorna NULL
-        {
-          risposta = "file ok";
-        } 
-        else 
-        {
-          risposta = "file già esistente";
+        while(spazioOccupato + newFile->length > spazio)
+        { //deve iniziare ad espellere file
+          fprintf(stderr, "il server è pieno (di spazio)\n");
+          fileRam *fileramtmptrash = pop(&queueFiles);
+          fprintf(stderr, "Sto espellendo il file %s dal server\n", fileramtmptrash->nome);
+          spazioOccupato-=fileramtmptrash->length;
+            //vanno fatte FREE
         }
-        fprintf(stderr,"risposta = %s\n",risposta);
-        lenRisposta = strlen(risposta);
-        if (writen(connfd, &lenRisposta, sizeof(int))<=0) { perror("ERRORE SCRITTURA NUMERO SERVER"); }     //vado a scrivere sul socket del client che ho trovato o meno il file nella coda
-        if (writen(connfd, risposta, lenRisposta * sizeof(char))<=0) { perror("ERRORE SCRITTURA RISPOSTA SERVER"); }
-        //fprintf(stderr, "ho scritto\n");
-        if(!esiste) 
-        {
-          fileRam *newfile = malloc(sizeof(fileRam));
-          newfile->nome = malloc(sizeof(char) * strlen(parametro));
-          strcpy(newfile->nome, parametro);
-          
-          //fprintf(stderr, "sto scrivendo\n");
-          SYSCALL_EXIT("readn", notused, readn(connfd, &(newfile->length), sizeof(int)), "read", "");//leggo la lunghezza del file 
-          newfile->buffer = malloc(sizeof(char) * newfile->length);
-          SYSCALL_EXIT("readn", notused, readn(connfd, newfile->buffer, (newfile->length)*sizeof(char)), "read", "");//leggo il contenuto del file
-          
-          //if (readn(connfd, newfile->buffer, (newfile->length)*sizeof(char))<=0) { fprintf(stderr, "sbagliato2\n"); }
-          
-          fprintf(stderr, "length file %ld\n", newfile->length);
-          pthread_mutex_lock(&mutexQueueFiles);
-          push(&queueFiles, newfile);           //inserisco il file nella code del server
-          pthread_mutex_unlock(&mutexQueueFiles);
-          risposta = "file inserito";
+        spazioOccupato+=newFile->length;
 
-          //fprintf(stderr, "risposta %s\n", risposta);
-          printQueueFiles(queueFiles);
-
-          lenRisposta = strlen(risposta);
+        newFile->buffer = malloc(sizeof(char) * newFile->length);//contenuto del file da leggere 
+        SYSCALL_EXIT("readn", notused, readn(connfd, newFile->buffer, (newFile->length)*sizeof(char)), "read", "");//leggo il contenuto del file
+        int risposta = 0;//successo  
+        
+        fprintf(stderr, "risposta %d\n", risposta);
+        printQueueFiles(queueFiles);//debug
           
-          if (writen(connfd, &lenRisposta, sizeof(int))<=0) { perror("ERRORE SCRITTURA RISPOSTA SERVER"); } //scrivo nel client il risultato dell'operazione 
-          if (writen(connfd, risposta, lenRisposta * sizeof(char))<=0) { perror("ERRORE SCRITTURA RISPOSTA SERVER"); }
-        }
+        if (writen(connfd, &risposta, sizeof(int))<=0) { perror("ERRORE SCRITTURA RISPOSTA SERVER"); } //scrivo nel client il risultato dell'operazione
+        
         break;
       }
       case 'c'://remove file dalla coda
       {
-        int res;
+        int risposta;
         pthread_mutex_lock(&mutexQueueFiles);//prendo il mutex per vedere se il file esiste e se posso rimuoverlo
         Node* esiste = fileExistsServer(queueFiles, parametro);
-        if(esiste != NULL) 
+        
+        if(esiste == NULL)//in caso che non esistesse il file nel server
         {
-          res = removeFromQueue(&queueFiles, esiste);
+          pthread_mutex_unlock(&mutexQueueFiles);
+          risposta = -1;//errore
+          fprintf(stderr, "errore, file %s NON rimosso dal server (non esisteva)\n", parametro);
+        } 
+        else//il file non esiste nella coda
+        {
+          fileRam *tmpfileramtrash = esiste->data;
+          spazioOccupato-= tmpfileramtrash->length;
+          risposta = removeFromQueue(&queueFiles, esiste);
+          
           fprintf(stderr, "file %s rimosso con successo dal server\n", parametro);
           pthread_mutex_unlock(&mutexQueueFiles);
-        } 
-        else//in caso che non esistesse il file nel server
-        {
-          pthread_mutex_unlock(&mutexQueueFiles);
-          fprintf(stderr, "errore, file %s NON rimosso dal server (non esisteva)\n", parametro);
-          res = 0;
+          
         }
-        //printQueueFiles(queueFiles); debug
-        if (writen(connfd, &res, sizeof(int))<=0) { perror("ERRORE RISPOSTA SERVER"); }
+        printQueueFiles(queueFiles);// debug
+        if (writen(connfd, &risposta, sizeof(int))<=0) { perror("ERRORE RISPOSTA SERVER"); }
         break;
+      }
+      case 'r': 
+      {
+        fprintf(stderr, "Ho ricevuto un comando di lettura!\n");
+        pthread_mutex_lock(&mutexQueueFiles);
+        Node* esiste = fileExistsServer(queueFiles, parametro);
+        int len; //RISPOSTA che ci dice se è tutto ok o no
+        if(esiste != NULL) 
+        { //file esistente nel server
+          fileRam *filetmp = esiste->data;
+          len = filetmp->length;
+          char* buf = filetmp->buffer;
+          //fprintf(stderr, "sto inserendo %d\n", len);
+          if (writen(connfd, &len, sizeof(int))<=0) { perror("ERRORE LUNGHEZZA"); }//leggo il contenuto del file
+          if (writen(connfd, buf, len * sizeof(char))<=0) { perror("ERRORE LETTURA CONTENUTO FILE"); }
+          fprintf(stderr, "file %s letto con successo dal server\n", parametro);
+          pthread_mutex_unlock(&mutexQueueFiles);
+        } 
+        else
+        { //ERRORE: file non trovato nel server
+          pthread_mutex_unlock(&mutexQueueFiles);
+          len = -1;//risposta
+          if (writen(connfd, &len, sizeof(int))<=0) { perror("c"); }
+          fprintf(stderr, "errore, file %s NON letto dal server (non esisteva)\n", parametro);
+        }
+        break;
+      }
+      case 'R': 
+      {
+        fprintf(stderr, "Ho ricevuto un comando readNFiles con n = %d\n", atoi(parametro));
+        int numDaLeggere;//numero dei file da leggere
+
+        if(atoi(parametro) > queueFiles->len)
+          numDaLeggere = queueFiles->len;
+        else
+          numDaLeggere = atoi(parametro);
+        if(numDaLeggere <= 0)
+          numDaLeggere = queueFiles->len;
+        if (writen(connfd, &numDaLeggere, sizeof(int))<=0) { perror("ERRORE SCRITTURA NUM FILE"); }
+        Node* nodetmp = queueFiles->head;
+        fileRam *fileramtmp;
+        char* buftmp;
+        for(int i = 0; i < numDaLeggere; i++)//salvo i nomi dei file in un array per poi leggerli in un secondo momento 
+        {
+          fileramtmp = nodetmp->data;
+          buftmp = malloc(sizeof(char) * strlen(fileramtmp->nome));
+          strcpy(buftmp, fileramtmp->nome);
+          int buftmplen = strlen(buftmp);
+          if (writen(connfd, &buftmplen, sizeof(int))<=0) { perror("c"); }
+          if (writen(connfd, buftmp, buftmplen * sizeof(char))<=0) { perror("x"); }
+          fprintf(stderr, "sto mandando il file %s al client\n", buftmp);
+          free(buftmp);
+          nodetmp = nodetmp->next;
+        }
+        break;
+      }
+      case 'e': 
+      { //openFile
+        fprintf(stderr, "ho ricevuto un comando di esistenza di un file %s nel server\n", parametro);
+        int risposta;
+        pthread_mutex_lock(&mutexQueueFiles);
+        Node* esiste = fileExistsServer(queueFiles, parametro);
+        
+        int flags;
+        if (readn(connfd, &flags, sizeof(int))<=0) { fprintf(stderr, "ERRORE LETTURA FLAGS\n"); }
+        fprintf(stderr, "codice flags %d\n", flags);
+        if(esiste == NULL && flags == 0) //deve aprire il file ma non esiste, errore
+          risposta = -1;
+        else 
+          if(esiste == NULL && flags == 1) 
+          { //deve creare e aprire il file (che non esiste)
+            if(queueFiles->len + 1 > numeroFile) 
+            { //deve iniziare ad espellere file per il numero file
+              fprintf(stderr, "il server è pieno (di numero), cancello un file\n");
+              fileRam *fileramtmptrash = pop(&queueFiles);
+              fprintf(stderr, "Sto espellendo il file %s dal server\n", fileramtmptrash->nome);
+
+              //vanno fatte FREE
+            }
+            //creo un file vuoto
+            fileRam *newfile = malloc(sizeof(fileRam));
+            newfile->nome = malloc(sizeof(char) * (strlen(basename(parametro)) + 1));//uso il base name per avere solo il nome del file senza path assoluto
+            strcpy(newfile->nome, basename(parametro));
+            newfile->nome[strlen(basename(parametro))] = '\0';
+            newfile->length = 0;
+            newfile->buffer = NULL;
+            push(&queueFiles, newfile);
+            risposta = 0;//successo
+          }
+          else 
+            if(esiste != NULL && flags == 0) 
+            { //deve aprire il file, che esiste già
+              risposta = 0;
+            }
+            else
+             if(esiste != NULL && flags == 1) 
+             { //deve creare e aprire il file, che esiste già, errore
+                risposta = -1;
+             }
+              pthread_mutex_unlock(&mutexQueueFiles);
+              if (writen(connfd, &risposta, sizeof(int))<=0) { perror("c"); }
       }
     }
 
@@ -294,7 +406,8 @@ int main(int argc, char* argv[])
   // tengo traccia del file descriptor con id piu' grande
   int fdmax = listenfd;
 
-  for(int i = 0; i < numWorkers; i++) {
+  for(int i = 0; i < numWorkers; i++) 
+  {
     FD_SET(p[i][0], &set); //p array di pipe, aggiungo tutte le pipe alla set in lettura
     if(p[i][0] > fdmax)
       fdmax = p[i][0];
@@ -303,10 +416,12 @@ int main(int argc, char* argv[])
 
   
   
-  for(;;) {
+  for(;;) 
+  {
 // copio il set nella variabile temporanea per la select
     tmpset = set;
-    if (select(fdmax+1, &tmpset, NULL, NULL, NULL) == -1) {
+    if (select(fdmax+1, &tmpset, NULL, NULL, NULL) == -1) 
+    {
       perror("select");
       return -1;
     }
@@ -368,9 +483,10 @@ int main(int argc, char* argv[])
 
         ComandoClient *cmdtmp = malloc(sizeof(ComandoClient));
         cmdtmp->comando = str.comando;
-        cmdtmp->parametro = malloc(sizeof(char) * strlen(str.arg));
+        cmdtmp->parametro = malloc(sizeof(char) * (strlen(str.arg)+1));
         cmdtmp->connfd = connfd;
         strcpy(cmdtmp->parametro, str.arg);
+        cmdtmp->parametro[strlen(str.arg)] = '\0';
 
         pthread_mutex_lock(&mutexQueueClient);
         push(&queueClient, cmdtmp);
